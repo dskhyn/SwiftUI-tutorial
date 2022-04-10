4e559bc6c03bd3f404fabf833c995281deefef40
dskhyn <dskhyn@gmail.com>
2022-04-10 20:39:01 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by dskhyn on 2022/04/10.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
